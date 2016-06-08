44e6d831f856c3160a4f4ca469052d081601810d
Enrique Argenis Vergara Hernandez <Enrique@macbook-air-de-enrique.local>
2016-06-08 14:21:52 -0500

    Buttons worked.

--- lab03/Start.h
+++ lab03/Start.h
@@ -10,7 +10,97 @@
 
 @interface Start : UIViewController{
     IBOutlet UIScrollView *scroller;
+    IBOutlet UIScrollView *scroller1;
+    
 }
+
+@property (weak, nonatomic) IBOutlet UIButton *btn_l1;
+
+@property (weak, nonatomic) IBOutlet UIButton *btn_d1;
+
+
+/*Buttons pressed*/
+
+
+- (IBAction)btn_l1Press:(id)sender;
+
+- (IBAction)btn_d1Press:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIImageView *icn1_f1;
+
+
+
+- (IBAction)btn_l2Press:(id)sender;
+
+- (IBAction)btn_d2Press:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIImageView *icn1_f2;
+
+
+
+- (IBAction)btn_l3Press:(id)sender;
+
+- (IBAction)btn_d3Press:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIImageView *icn1_f3;
+
+
+
+- (IBAction)btn_l4Press:(id)sender;
+
+- (IBAction)btn_d4Press:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIImageView *icn1_f4;
+
+
+
+- (IBAction)btn_l5Press:(id)sender;
+
+- (IBAction)btn_d5Press:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIImageView *icn1_f5;
+
+
+
+
+- (IBAction)btn_l6Press:(id)sender;
+
+- (IBAction)btn_d6Press:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIImageView *icn1_f6;
+
+
+- (IBAction)btn_l7Press:(id)sender;
+
+- (IBAction)btn_d7Press:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIImageView *icn1_f7;
+
+
+
+- (IBAction)btn_l8Press:(id)sender;
+
+- (IBAction)btn_d8Press:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIImageView *icn1_f8;
+
+
+
+- (IBAction)btn_l9Press:(id)sender;
+
+- (IBAction)btn_d9Press:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIImageView *icn1_f9;
+
+
+
+- (IBAction)btn_l10Press:(id)sender;
+
+- (IBAction)btn_d10Press:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIImageView *icn1_f10;
+
+
 
 
 @end
--- lab03/Start.m
+++ lab03/Start.m
@@ -18,13 +18,90 @@
     [super viewDidLoad];
     [scroller setScrollEnabled:YES];
     [scroller setContentSize:CGSizeMake(320, 1000)];
+    [scroller1 setScrollEnabled:YES];
+    [scroller1 setContentSize:CGSizeMake(320, 1000)];
 }
+
+/*----------------------------------------*/
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+/*----------------------------------------*/
 
+/*Button methods*/
+/*----------------------------------------*/
 
+- (IBAction)btn_l1Press:(id)sender {
+    self.icn1_f1.image=[UIImage imageNamed:@"icn_selected_v.png"];
+}
+
+- (IBAction)btn_d1Press:(id)sender {
+    self.icn1_f1.image=[UIImage imageNamed:@"icn_selected_m.png"];
+}
+- (IBAction)btn_l2Press:(id)sender {
+    self.icn1_f2.image=[UIImage imageNamed:@"icn_selected_v.png"];
+}
+
+- (IBAction)btn_d2Press:(id)sender {
+    self.icn1_f2.image=[UIImage imageNamed:@"icn_selected_m.png"];
+}
+- (IBAction)btn_l3Press:(id)sender {
+    self.icn1_f3.image=[UIImage imageNamed:@"icn_selected_v.png"];
+}
+
+- (IBAction)btn_d3Press:(id)sender {
+    self.icn1_f3.image=[UIImage imageNamed:@"icn_selected_m.png"];
+}
+- (IBAction)btn_l4Press:(id)sender {
+    self.icn1_f4.image=[UIImage imageNamed:@"icn_selected_v.png"];
+}
+
+- (IBAction)btn_d4Press:(id)sender {
+    self.icn1_f4.image=[UIImage imageNamed:@"icn_selected_m.png"];
+}
+- (IBAction)btn_l5Press:(id)sender {
+    self.icn1_f5.image=[UIImage imageNamed:@"icn_selected_v.png"];
+}
+
+- (IBAction)btn_d5Press:(id)sender {
+    self.icn1_f5.image=[UIImage imageNamed:@"icn_selected_m.png"];
+}
+- (IBAction)btn_l6Press:(id)sender {
+    self.icn1_f6.image=[UIImage imageNamed:@"icn_selected_v.png"];
+}
+
+- (IBAction)btn_d6Press:(id)sender {
+    self.icn1_f6.image=[UIImage imageNamed:@"icn_selected_m.png"];
+}
+- (IBAction)btn_l7Press:(id)sender {
+    self.icn1_f7.image=[UIImage imageNamed:@"icn_selected_v.png"];
+}
+
+- (IBAction)btn_d7Press:(id)sender {
+    self.icn1_f7.image=[UIImage imageNamed:@"icn_selected_m.png"];
+}
+- (IBAction)btn_l8Press:(id)sender {
+    self.icn1_f8.image=[UIImage imageNamed:@"icn_selected_v.png"];
+}
+
+- (IBAction)btn_d8Press:(id)sender {
+    self.icn1_f8.image=[UIImage imageNamed:@"icn_selected_m.png"];
+}
+- (IBAction)btn_l9Press:(id)sender {
+    self.icn1_f9.image=[UIImage imageNamed:@"icn_selected_v.png"];
+}
+
+- (IBAction)btn_d9Press:(id)sender {
+    self.icn1_f9.image=[UIImage imageNamed:@"icn_selected_m.png"];
+}
+- (IBAction)btn_l10Press:(id)sender {
+    self.icn1_f10.image=[UIImage imageNamed:@"icn_selected_v.png"];
+}
+
+- (IBAction)btn_d10Press:(id)sender {
+    self.icn1_f10.image=[UIImage imageNamed:@"icn_selected_m.png"];
+}
 @end
